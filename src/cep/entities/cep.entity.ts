626cf42960bfc64e6bc597d13a208545720d752b
Carlos Cerqueira <ce.cesc01@gmail.com>
2022-04-18 20:08:21 -0300

    feat: Add cep entity

---
+++ src/cep/entities/cep.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class CEP {
+  @PrimaryColumn()
+  cep: string;
+
+  @Column()
+  logradouro: string;
+
+  @Column()
+  complemento: string;
+
+  @Column()
+  bairro: string;
+
+  @Column()
+  localidade: string;
+
+  @Column()
+  uf: string;
+
+  @Column()
+  ibge: string;
+
+  @Column()
+  gia: string;
+
+  @Column()
+  ddd: string;
+
+  @Column()
+  siafi: string;
+}
