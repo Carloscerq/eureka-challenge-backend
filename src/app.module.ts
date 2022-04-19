2cdb36ff2ebe5619ca75427fec509b31e317979f
Carlos Cerqueira <ce.cesc01@gmail.com>
2022-04-19 00:31:06 -0300

    feat: Add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CepModule } from './cep/cep.module';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
 
 @Module({
-  imports: [CepModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: false,
+      entities: ['dist/**/*entity.js'],
+      migrations: ['dist/migration/*.js'],
+      subscribers: ['src/subscriber/**/*.ts'],
+    }),
+    CepModule,
+  ],
   controllers: [],
   providers: [],
 })
