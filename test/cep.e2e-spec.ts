4b4567ecf5a11c1c2e9ed05d56b03d0e7d75285b
Carlos Cerqueira <ce.cesc01@gmail.com>
2022-04-19 23:07:26 -0300

    test: add e2e test

--- test/cep.e2e-spec.ts
+++ test/cep.e2e-spec.ts
@@ -3,10 +3,10 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Cep (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,22 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/41830498').expect(200).expect({
+      cep: '41830498',
+      logradouro: 'Rua Tenente Fernando Tuy',
+      complemento: '',
+      bairro: 'Pituba',
+      localidade: 'Salvador',
+      uf: 'BA',
+      ibge: '2927408',
+      gia: '',
+      ddd: '71',
+      siafi: '3849',
+    });
   });
 });
